05bd0f8bad116eee2983271f5f648bbdc74125c3
Bartek Bugała <bartek.bugala@gmail.com>
2019-09-03 00:52:18 +0200

    add weboack env to config and package.json

--- Kodilla-20.5/webpack.config.js
+++ Kodilla-20.5/webpack.config.js
@@ -1,29 +1,38 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader'},
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
+module.exports = env => {
+    const enviroment = env || 'production'
+    console.log('Enviroment:' + env);
+    const optimization = (enviroment === 'production') ? true : false;
+    return {
+        mode: enviroment,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + enviroment + '.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
                         }
-                    }
-                ]
-            }
-        ]
+                    ]
+                }
+            ]
+        },
+        optimization: {
+            minimize: optimization
+        }
     }
 };

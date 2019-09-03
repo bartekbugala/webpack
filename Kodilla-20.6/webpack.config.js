527bc9541b8d2657fece6db1dfb9ee2194f52f5a
Bartek Bugała <bartek.bugala@gmail.com>
2019-09-03 19:22:29 +0200

    initial commit

---
+++ Kodilla-20.6/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = env => {
+    const enviroment = env || 'production'
+    console.log('Enviroment:' + env);
+    return {
+        mode: enviroment,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + enviroment + '.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+    }
+};

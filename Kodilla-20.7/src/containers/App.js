16420ffea050aa38ac4717fd65454945f032c75e
Bartek Bugała <bartek.bugala@gmail.com>
2019-09-05 19:24:41 +0200

    edit: App.css, App.js package.json, webpack.config

--- Kodilla-20.7/src/containers/App.js
+++ Kodilla-20.7/src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import { hot } from 'react-hot-loader';
 import Title from '../components/Title.js';
 import TodoList from '../components/TodoList.js';
 
@@ -52,4 +53,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default hot(module)(App);

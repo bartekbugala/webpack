75f7db2fc67928824dcabc409b5efa6eed1f6906
Bartek Bugała <bartek.bugala@gmail.com>
2019-09-07 16:56:59 +0200

    sass + cookie implementation

--- Kodilla-20.7/src/containers/App.js
+++ Kodilla-20.7/src/containers/App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import uuid from 'uuid';
-import style from './App.css';
+//import style from './App.css';
+import style from '../sass/style.scss';
 import TodoForm from '../components/TodoForm.js';
 import Title from '../components/Title.js';
 import TodoList from '../components/TodoList.js';
 import { hot } from 'react-hot-loader';
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    const { cookies } = props;
+    if((cookies.get('data')) === undefined) {
+      cookies.set('data', [], { path: '/', maxAge:  15 });
+    }
     this.state = {
       appTitle: 'To do List',
-      data: []
+      data: cookies.get('data')
     };
     this.addTodo = this.addTodo.bind(this);
     this.removeTodo = this.removeTodo.bind(this);
   }
   addTodo(val) {
+    const { cookies } = this.props;
     const todo = {
       name: val || 'default',
       id: uuid.v4()
     };
     const data = [...this.state.data, todo];
+    cookies.set('data', data, { path: '/', maxAge: 5184000 });
     this.setState({ data });
   }
   removeTodo(id) {
+    const { cookies } = this.props;
     const remainder = this.state.data.filter(todo => todo.id !== id);
+    cookies.set('data', remainder, { path: '/', maxAge: 5184000 });
     this.setState({ data: remainder });
   }
   render() {
@@ -39,4 +50,9 @@ class App extends React.Component {
   }
 }
 
-export default hot(module)(App);
+ App.propTypes = {
+  cookies: instanceOf(Cookies).isRequired
+};
+
+/* export default hot(module)(App); */
+export default hot(module) (withCookies(App));
--- Kodilla-20.7/src/index.js
+++ Kodilla-20.7/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { CookiesProvider } from 'react-cookie';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+<CookiesProvider>
+<App />
+</CookiesProvider>, document.getElementById('app'));
